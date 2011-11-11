4546e0b2a863a2880f5b8ad60a25fb408862c179
dcapwell <dcapwell@yahoo-inc.com>
2011-11-11 10:58:02 -0800

    moved giraph-yarn out of normal giraph package.  Fixed tests to load autogenerated classpath so yarn works.

--- giraph-yarn/src/main/java/org/apache/giraph/yarn/GiraphApplicationMaster.java
+++ giraph-yarn/src/main/java/org/apache/giraph/yarn/GiraphApplicationMaster.java
@@ -1,5 +1,7 @@
 package org.apache.giraph.yarn;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.net.NetUtils;
 import org.apache.hadoop.yarn.api.AMRMProtocol;
@@ -11,6 +13,7 @@ import org.apache.hadoop.yarn.ipc.YarnRPC;
 import org.apache.hadoop.yarn.util.ConverterUtils;
 
 import java.net.InetSocketAddress;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,24 +23,19 @@ import java.net.InetSocketAddress;
  * To change this template use File | Settings | File Templates.
  */
 public class GiraphApplicationMaster {
-//    private static final Log LOG = LogFactory.getLog(GiraphApplicationMaster.class.getName());
+    private static final Log LOG = LogFactory.getLog(GiraphApplicationMaster.class.getName());
 
     public static void main(String[] args) {
-//        LOG.info("Launched Giraph App Master");
-//
-//        Map<String, String> envs = System.getenv();
-//        LOG.info("Environment Variables");
-//        for(String key : envs.keySet()) {
-//            LOG.info(key + " => " + envs.get(key));
-//        }
+        LOG.info("Launched Giraph App Master");
+
+        Map<String, String> envs = System.getenv();
+        LOG.info("Environment Variables");
+        for(String key : envs.keySet()) {
+            LOG.info(key + " => " + envs.get(key));
+        }
 
         GiraphApplicationMaster appMaster = new GiraphApplicationMaster();
         appMaster.run();
-
-//        LOG.info("Application master for app"
-//        + ", appId=" + appAttemptID.getApplicationId().getId()
-//        + ", clustertimestamp=" + appAttemptID.getApplicationId().getClusterTimestamp()
-//        + ", attemptId=" + appAttemptID.getAttemptId());
 
         System.exit(0);
     }
@@ -50,10 +48,16 @@ public class GiraphApplicationMaster {
 
     public GiraphApplicationMaster() {
         this.conf = new Configuration();
+        this.conf.addResource("job.xml");
         rpc = YarnRPC.create(conf);
 
         ContainerId containerId = ConverterUtils.toContainerId(System.getenv(ApplicationConstants.AM_CONTAINER_ID_ENV));
         ApplicationAttemptId appAttemptID = containerId.getApplicationAttemptId();
+
+        LOG.info("Application master for app"
+        + ", appId=" + appAttemptID.getApplicationId().getId()
+        + ", clustertimestamp=" + appAttemptID.getApplicationId().getClusterTimestamp()
+        + ", attemptId=" + appAttemptID.getAttemptId());
     }
 
     public void run() {
